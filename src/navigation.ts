15bfa25d54854d7d69993019d8999952c191d4cc
Yves Thorrez <yves.thorrez@gmail.com>
2025-03-22 09:41:00 +0100

    Update content

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,7 +3,7 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Home',
       links: [
         {
           text: 'SaaS',
@@ -177,7 +177,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="./src/assets/images/spectre.webp" alt="onWidget logo" loading="lazy"></img>
+    Created by <a class="text-blue-600 underline dark:text-muted" href="https://spectre-hd.eu/"> SPECTRE-HD</a> · All rights reserved.
   `,
 };
